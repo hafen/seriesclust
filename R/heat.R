5b36eb029aad40b2360d7c58ebdafdad70d18741
hafen <rhafen@gmail.com>
2017-05-05 13:23:02 -0700

    Add pheatmap options and update its print method

--- R/heat.R
+++ R/heat.R
@@ -18,8 +18,11 @@ plot_heat <- function(x, k, col, cutoff = 1, color = viridis::magma,
   xaxis_height = NULL, yaxis_width = NULL, interactive = TRUE, ...) {
 
   dat <- join_data_with_cluster(x, k)
-
   clmns <- c(col, "cluster_name")
+
+  # if (!(scale %in% c("row", "column")))
+  #   stop("scale argument should take values 'row' or 'column'")
+  # scale_var <- ifelse(scale == "row", col, "cluster_name")
 
   tmp <- dat %>%
     dplyr::group_by_(.dots = union(clmns, x$group_vars)) %>%
@@ -30,7 +33,7 @@ plot_heat <- function(x, k, col, cutoff = 1, color = viridis::magma,
     dplyr::ungroup() %>%
     dplyr::group_by_(col) %>%
     dplyr::mutate_(pct_cluster = ~ 100 * n / sum(n)) %>%
-    dplyr::arrange_(col)
+    dplyr::arrange_(col, "cluster_name")
 
   tmp2 <- tidyr::crossing_(as.list(tmp[clmns]))
   tmp <- suppressMessages(dplyr::left_join(tmp2, tmp)) %>%
@@ -65,12 +68,63 @@ plot_heat <- function(x, k, col, cutoff = 1, color = viridis::magma,
       xaxis_height <- mnc_col * 40
     }
 
-    d3heatmap::d3heatmap(round(mt, 1), colors = color, cellnote = mt2,
+    res <- d3heatmap::d3heatmap(round(mt, 1), colors = color, cellnote = mt2,
       yaxis_width = yaxis_width, xaxis_height = xaxis_height, ...)
   } else {
-    pheatmap::pheatmap(mt,
-      color = color,
-      annotation_colors = "red",
-      border_color = "#ffffff50", ...)
+    pars <- list(...)
+    if (is.null(pars$annotation_colors))
+      pars$annotation_colors <- "red"
+    if (is.null(pars$border_color))
+      pars$border_color <- "#ffffff50"
+    if (is.null(pars$number_color))
+      pars$number_color <- "#ffffff50"
+    if (is.null(pars$number_format))
+      pars$number_format <- "%.0f%%"
+    if (is.null(pars$color))
+      pars$color <- color
+    if (is.null(pars$xlab))
+      pars$xlab <- "cluster"
+    if (is.null(pars$ylab))
+      pars$ylab <- col
+    pars$silent <- TRUE
+    pars$mat <- mt
+    obj <- do.call(pheatmap::pheatmap, pars)
+    pars$silent <- FALSE
+
+    res <- list(obj = obj, pars = pars)
+    class(res) <- c("pheatmap_plot", class(res))
   }
+  res
 }
+
+#' Print trellis plot to servr
+#'
+#' @param x pheatmap_plot
+#' @param \ldots additional parameters
+#' @importFrom grid grid.newpage grid.draw
+#' @export
+print.pheatmap_plot <- function(x, ...) {
+  ww <- hh <- 1
+  xlab <- x$pars$xlab
+  ylab <- x$pars$ylab
+  x$pars$xlab <- NULL
+  x$pars$ylab <- NULL
+  setHook("grid.newpage", function()
+    grid::pushViewport(grid::viewport(x = 1, y = 1,
+      width = ifelse(is.null(ylab), 1, 0.95),
+      height = ifelse(is.null(xlab), 1, 0.95),
+      name = "vp", just = c("right", "top"))),
+    action = "prepend")
+  do.call(pheatmap::pheatmap, x$pars)
+  setHook("grid.newpage", NULL, "replace")
+  if (!is.null(xlab))
+    grid::grid.text(xlab, y = -0.02, gp = grid::gpar(fontsize = 14))
+  if (!is.null(ylab))
+    grid::grid.text(ylab, x = -0.02, rot = 90, gp = grid::gpar(fontsize = 14))
+}
+
+# plot_heat(km, 9, col = "sector", interactive = FALSE,
+#   display_numbers = TRUE, cutree_cols = 3, cutree_rows = 3, annotation_col = tmpd, annotation_colors = list(group = c("1" = "red", "2" = "green", "3" = "blue")))
+# tmp <- cutree(p$obj$tree_col, 3)
+# tmpd <- data.frame(group = as.character(tmp), stringsAsFactors = FALSE)
+# rownames(tmpd) <- names(tmp)
